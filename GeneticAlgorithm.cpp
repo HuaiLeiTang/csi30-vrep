807b3cfa4acc755f7b65e1b88ee45c37ac68de61
Thyago Stall <thstall@gmail.com>
2018-09-15 16:56:26 -0300

    First version of GA

--- GeneticAlgorithm.cpp
+++ GeneticAlgorithm.cpp
@@ -1,21 +1,170 @@
-//
-// Created by Thyago Stall on 14/09/2018.
-//
+//Bruno Clemente e Thyago Stall
+#include <random>
+#include <map>
 
 #include "GeneticAlgorithm.hpp"
+#include "Map.hpp"
+#include "AStarSearch.hpp"
+
+const int INF = 10000;
+const int POPULATION_SIZE = 10;
+
+bool compare(const std::pair<int, Point> &a,const std::pair<int, Point> &b){
+    return a.first < b.first;
+}
 
 std::vector<State> GeneticAlgorithm::find() {
-    return std::vector<State>();
+    int generation = 0;
+    std::vector<Point> population = this->initializePopulation();
+    int currentEvaluation = this->evaluatePopulation(population);
+    printf("currentEvaluation -> %d\n", currentEvaluation);
+
+    while (currentEvaluation >= INF) {
+        generation++;
+        population = this->createNewGeneration(population);
+        currentEvaluation = this->evaluatePopulation(population);
+        printf("currentEvaluation -> %d\n", currentEvaluation);
+    }
+
+    Point best = this->select(population);
+    return this->findPathGoingThrough(best);
 }
 
-int GeneticAlgorithm::fitness(const State &state) {
-    return 0;
+std::vector<State> GeneticAlgorithm::findPathGoingThrough(Point best) {
+    State best_state {best.x, best.y, 0};
+
+    std::vector<State> endBestState;
+    endBestState.push_back(best_state);
+
+    AStarSearch finderFirstHalf {this->start_state, endBestState};
+    std::vector<State> firstHalf = finderFirstHalf.find();
+
+    AStarSearch finderSecondHalf {best_state, this->end_states};
+    std::vector<State> secondHalf = finderSecondHalf.find();
+
+    firstHalf.insert(firstHalf.end(), secondHalf.begin(), secondHalf.end());
+    return firstHalf;
 }
 
-State GeneticAlgorithm::mutate(const State &state) {
-    return state;
+std::vector<Point> GeneticAlgorithm::initializePopulation() {
+    std::vector<Point> population;
+    std::random_device rd;
+
+    for (int i = 0; i < POPULATION_SIZE; i++) {
+        int x = rd() % 10;
+        int y = rd() % 10;
+        population.push_back({x, y});
+        printf("-> {%d, %d}\n", x, y);
+    }
+    return population;
 }
 
-std::pair<State, State> GeneticAlgorithm::crossover(const State &parent1, const State &parent2) {
-    return std::pair<State, State>{parent1, parent2};
+int GeneticAlgorithm::evaluatePopulation(const std::vector<Point> &points) {
+    int result = 0;
+    for (auto &point : points) {
+        result += this->fitness(point);
+    }
+    return result;
+}
+
+std::vector<Point> GeneticAlgorithm::createNewGeneration(const std::vector<Point> &population) {
+    std::vector<Point> newGeneration;
+
+    for (int i = 0; i < population.size(); i += 2) {
+        Point parent1 = this->select(population);
+        Point parent2 = this->select(population);
+
+        std::pair<Point, Point> offspring = this->crossover(parent1, parent2);
+
+        Point offspring1 = this->mutate(offspring.first);
+        Point offspring2 = this->mutate(offspring.second);
+
+        newGeneration.push_back(offspring1);
+        newGeneration.push_back(offspring2);
+    }
+
+    return newGeneration;
+}
+
+Point GeneticAlgorithm::select(const std::vector<Point> &population) {
+    std::vector<std::pair<int, Point>> candidates;
+    for (auto &point : population) {
+        candidates.push_back(std::make_pair(this->fitness(point), point));
+    }
+    std::sort(candidates.begin(), candidates.end(), compare);
+
+    auto it = candidates.begin();
+    std::random_device rd;
+    std::advance(it, rd() % candidates.size());
+    return it->second;
+}
+
+int GeneticAlgorithm::fitness(const Point &point) {
+    int distanceToStartState = this->distance(State::getStartState(), point);
+    int minimumDistanceToAnEndState = INF;
+    for (auto &endState : State::getEndState()) {
+        minimumDistanceToAnEndState = std::min(minimumDistanceToAnEndState, this->distance(endState, point));
+        if (this->areThereWallsInPath(point, endState)) {
+            return INF;
+        }
+    }
+    return abs(distanceToStartState - minimumDistanceToAnEndState);
+}
+
+bool GeneticAlgorithm::areThereWallsInPath(const Point &p1, const State &p2) {
+    int x = p1.x;
+    int y = p1.y;
+
+    int x2 = p2.getX();
+    int y2 = p2.getY();
+
+    int w = x2 - x;
+    int h = y2 - y;
+    int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+
+    if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+    if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+    if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+
+    int longest = abs(w);
+    int shortest = abs(h);
+
+    if (longest <= shortest) {
+        longest = abs(h);
+        shortest = abs(w);
+        if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+        dx2 = 0;
+    }
+
+    int numerator = longest >> 1;
+    for (int i = 0; i <= longest; i++) {
+        if (Map::isObstacle(x, y))
+            return true;
+
+        numerator += shortest;
+        if (numerator >= longest) {
+            numerator -= longest;
+            x += dx1; y += dy1;
+        } else {
+            x += dx2; y += dy2;
+        }
+    }
+
+    return false;
+}
+
+int GeneticAlgorithm::distance(const State &state, const Point &point) {
+    return (int) sqrt(pow(point.x - state.getX(), 2) + pow(point.y - state.getY(), 2));
+}
+
+Point GeneticAlgorithm::mutate(const Point &point) {
+    int increment[] = {-1, 0, 1};
+    std::random_device rd;
+    return {point.x + increment[rd() % 3], point.y + increment[rd() % 3]};
+}
+
+std::pair<Point, Point> GeneticAlgorithm::crossover(const Point &parent1, const Point &parent2) {
+    Point offspring1 {parent2.x, parent1.y};
+    Point offspring2 {parent1.x, parent2.y};
+    return std::pair<Point, Point>{offspring1, offspring2};
 }
--- GeneticAlgorithm.hpp
+++ GeneticAlgorithm.hpp
@@ -4,13 +4,32 @@
 
 #include "PathFinder.hpp"
 
+struct Point {
+    int x;
+    int y;
+};
+
 class GeneticAlgorithm : public PathFinder {
 public:
+    GeneticAlgorithm(const State &start_state, const std::vector<State> &end_states) : PathFinder(start_state,
+                                                                                                  end_states) {}
+
     std::vector<State> find() override;
 private:
-    int fitness(const State &state);
-    State mutate(const State &state);
-    std::pair<State, State> crossover(const State &parent1, const State &parent2);
+    int fitness(const Point &point);
+    int distance(const State &state, const Point &point);
+    Point mutate(const Point &point);
+    std::pair<Point, Point> crossover(const Point &parent1, const Point &parent2);
+
+    std::vector<Point> createNewGeneration(const std::vector<Point> &population);
+    Point select(const std::vector<Point> &population);
+
+    std::vector<Point> initializePopulation();
+
+    int evaluatePopulation(const std::vector<Point> &points);
+    bool areThereWallsInPath(const Point &p1, const State &p2);
+
+    std::vector<State> findPathGoingThrough(Point best);
 };
 
 #endif //CPPREMOTEAPI_GENETICALGORITHM_HPP
